fbea7a2419cc1ef0d9690ee5a946da3041de3f81
Isak Midtvedt <s350289@oslomet.no>
2022-11-14 22:17:30 +0100

    testing

--- nav.js
+++ nav.js
@@ -1,7 +1,11 @@
 const navBar = document.querySelector(".primary-nav")
 const navToggle = document.querySelector(".nav-toggle")
 const navBars = document.querySelectorAll(".nav-bar")
+const navCircle = document.querySelector(".menu-circle")
+const navCircleBig = document.querySelector(".big")
+const listItem = document.querySelectorAll('li')
 const body = document.body
+
 
 
 
@@ -11,12 +15,21 @@ navToggle.addEventListener("click", () => {
     var width = body.clientWidth;
 
     body.classList.toggle("no-scroll")
+    
+    listItem.forEach(list => list.classList.toggle("opacity"))
 
+    navBars.forEach(element => element.classList.toggle("nav-close"))
+
+    
+    
     if (visible === "false" && width >= 1184) {
         navBar.setAttribute("visible-desktop", true)
-        console.log(width)
-    } else {
+        navCircle.classList.toggle("animate-circle")
+        navCircleBig.classList.toggle("animate-circle-big")
+    } else if(width >= 1184){
         navBar.setAttribute("visible-desktop", false)
+        navCircle.classList.toggle("animate-circle")
+        navCircleBig.classList.toggle("animate-circle-big")
     }
 
     if (visibleMobile === "false" && width <= 1183) {
