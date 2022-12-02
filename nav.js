21e01d9581223d5d016962aebf4e4019519087d5
Isak Midtvedt <s350289@oslomet.no>
2022-12-02 13:50:18 +0100

    a

--- nav.js
+++ nav.js
@@ -7,8 +7,6 @@ const listItem = document.querySelectorAll('li')
 const body = document.body
 
 
-
-
 navToggle.addEventListener("click", () => {
     const visible = navBar.getAttribute("visible-desktop")
     const visibleMobile = navBar.getAttribute("visible-mobile")
@@ -37,5 +35,28 @@ navToggle.addEventListener("click", () => {
         navBar.setAttribute("visible-mobile", true)
     } else {
         navBar.setAttribute("visible-mobile", false)
+    }
+})
+
+window.addEventListener("resize", function(event) {
+    var visibleM = navBar.getAttribute("visible-mobile")
+    var visibleD = navBar.getAttribute("visible-desktop")
+
+    console.log(visibleD, visibleM)
+
+    
+    if(body.clientWidth < 1184 && visibleD === "true") {
+        navBar.setAttribute("visible-desktop", false)
+        listItem.forEach(list => list.classList.toggle("opacity"))
+
+        navBars.forEach(element => element.classList.toggle("nav-close"))
+        navCircle.classList.toggle("animate-circle")
+        navCircleBig.classList.toggle("animate-circle-big")
+        
+    } else if (body.clientWidth >= 1184 && visibleM === "true") {
+        navBar.setAttribute("visible-mobile", false)
+        listItem.forEach(list => list.classList.toggle("opacity"))
+
+        navBars.forEach(element => element.classList.toggle("nav-close"))
     }
 })
